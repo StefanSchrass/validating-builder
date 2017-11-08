bb080b120c1ebcb941d54e893bdda9c06cbb51dc
StefanSchrass <stefan.schrass@gmail.com>
2017-11-08 18:07:36 +0100

    removed getDesignatedClass

--- src/main/java/de/sschrass/util/validatingbuilder/ValidatingBuilder.java
+++ src/main/java/de/sschrass/util/validatingbuilder/ValidatingBuilder.java
@@ -5,33 +5,42 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Optional;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "WeakerAccess"})
 @ParametersAreNonnullByDefault
 abstract public class ValidatingBuilder<T> {
     private static final Logger LOGGER = LoggerFactory.getLogger(ValidatingBuilder.class);
-    @SuppressWarnings("WeakerAccess")
     protected abstract Boolean isValid();
-    @SuppressWarnings("WeakerAccess")
-    protected abstract Class<T> getDesignatedClass();
 
     public Optional<T> build() {
         Optional<T> designated = Optional.empty();
         if (isValid()) {
             try {
-                final Optional<Constructor<T>> builderCtor = Optional.ofNullable(getDesignatedClass().getDeclaredConstructor(this.getClass()));
+                final Class<? extends ValidatingBuilder> builderClass = getClass();
+                final Class<T> designatedClass = designatedClassFromType(typeFromBuilderClass(builderClass));
+                final Optional<Constructor<T>> builderCtor = Optional.ofNullable(designatedClass.getDeclaredConstructor(builderClass));
                 if (builderCtor.isPresent()) {
                     builderCtor.get().setAccessible(true);
                     designated = Optional.of(builderCtor.get().newInstance(this));
                 }
             }
-            catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            catch (Exception e) {
                 LOGGER.error("Could not instantiate the designated object, due to {}", e.getMessage());
             }
         }
 
         return designated;
+    }
+
+    private Type typeFromBuilderClass(Class<? extends ValidatingBuilder> builderClass) {
+        return ((ParameterizedType) builderClass.getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    @SuppressWarnings("unchecked")
+    private Class<T> designatedClassFromType(final Type type) throws ClassNotFoundException {
+        return (Class<T>) Class.forName(type.getTypeName());
     }
 }
